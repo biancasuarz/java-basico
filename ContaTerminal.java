e5ed66165c0cd290821012701d28299d400f4d1e
biancasuarz <blancasuarz@gmail.com>
2023-08-20 15:56:10 -0300

    Projeto ContaBanco

---
+++ ContaTerminal.java
@@ -0,0 +1,9 @@
+public class ContaTerminal {
+
+        public void numero(){
+            int numero = 1021;
+        }
+
+        String agencia = "067-8";
+        String nome = "Mario Andrade";
+}
---
+++ Usuario.java
@@ -0,0 +1,24 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Usuario {
+
+    public class Usuario {
+        public static void main(String[] args) {
+
+            Locale.setDefault(Locale.US);
+            Scanner sc = new Scanner(System.in);
+
+            System.out.println("Por favor, digite o número da agência: ");
+            sc.nextInt();
+
+            System.out.println("Por favor, digite agora o número da sua conta: ");
+            sc.next();
+
+            System.out.println("Digite agora seu nome completo: ");
+            sc.next();
+
+            sc.close();
+        }
+    }
+}
